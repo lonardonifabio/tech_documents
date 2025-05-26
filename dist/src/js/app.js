71a68f5eee63b873520d9e0d5c1705099983c143
Fabio <lonardoni.fabio@gmail.com>
2025-05-26 17:04:01 +0200

    Update app.js

--- dist/src/js/app.js
+++ dist/src/js/app.js
@@ -94,7 +94,7 @@ const DocumentLibrary = () => {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        fetch('./dist/data/documents.json')
+        fetch('./data/documents.json')
             .then(response => response.json())
             .then(data => {
                 setDocuments(data);
